16de1a8f5e30dac78ff0c06deecb888b10f97fe8
Joseph Braide <braidej@gmail.com>
2023-07-16 08:08:06 +0100

    initialised API app

--- platform_api/platform_api/settings.py
+++ platform_api/platform_api/settings.py
@@ -37,6 +37,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # apps
+    'api',
+
+    # third party
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
---
+++ platform_api/platform_api/settings_dev.py
@@ -0,0 +1,14 @@
+from .settings import *
+
+DEBUG = True
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'nova_crypto',
+        'USER': 'crypto_admin',
+        'PASSWORD': 'novacryptotradingplatform',
+        'HOST': 'db',
+        'PORT': '',
+    }
+}
