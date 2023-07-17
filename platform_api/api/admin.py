a4439dddd6699b527195effe609667728a2f34da
Joseph Braide <braidej@gmail.com>
2023-07-17 06:30:01 +0100

    Add wallets to Admin

--- platform_api/api/admin.py
+++ platform_api/api/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from api.models import FiatWallet, CryptoWallet
 
-# Register your models here.
+@admin.register(FiatWallet)
+class FiatWalletAdmin(admin.ModelAdmin):
+    list_display = ['user', ]
+
+@admin.register(CryptoWallet)
+class CryptoWalletAdmin(admin.ModelAdmin):
+    list_display = ['user', 'wallet_type', 'balance']
