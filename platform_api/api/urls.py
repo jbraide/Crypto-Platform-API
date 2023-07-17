7ca41ee62dab2f2f7f81cef0480d7c03f30b36ab
Joseph Braide <braidej@gmail.com>
2023-07-17 06:32:33 +0100

    views and url for creating base wallet

--- platform_api/api/urls.py
+++ platform_api/api/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
+from .views import CreateBaseCryptoWallets
 
 app_name = 'platform-api'
 
 urlpatterns = [
-    
+    path('create-base-crypto-wallets/', CreateBaseCryptoWallets.as_view())    
 ]
--- platform_api/api/views.py
+++ platform_api/api/views.py
@@ -1,3 +1,63 @@
-from django.shortcuts import render
+from django.contrib.auth.models import User
+from rest_framework.generics import CreateAPIView, GenericAPIView
+from rest_framework.views import APIView
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
 
-# Create your views here.
+from .models import CryptoWallet
+from .serializers import CryptoWalletSerializer
+
+
+class CreateBaseCryptoWallets(GenericAPIView):
+    """
+    Create Wallets for 7 most popular Cryptos
+    """
+    serializer_class = CryptoWalletSerializer
+    queryset = CryptoWallet.objects.all()
+    permission_classes = [IsAuthenticated, ]
+
+    def post(self, request, *args, **kwargs):
+        try:
+            self.create_crypto_wallets(request.user)
+        except:
+            return Response(
+                data={
+                'status': 'error',
+                'message': 'You already Have Wallets created',
+            },
+            status=400
+            )
+        
+        return Response(
+            data={
+                'status': 'success',
+                'message': 'Wallet Created'
+            },
+            status=201
+        )
+
+    def create_crypto_wallets(self, user):
+        user = User.objects.get(username=user)
+
+        top_wallets = [
+            'Bitcoin',
+            'Ethereum',
+            'Ripple',
+            'LiteCoin',
+            'Bitcoin Cash',
+            'USD Coin',
+            'USD Tether',
+        ]
+
+        wallet_objects = [
+            CryptoWallet(
+                user=user,
+                wallet_type=wallet,
+                balance=0.0, 
+                address='', 
+            )
+            for wallet in top_wallets
+        ] 
+        
+        CryptoWallet.objects.bulk_create(wallet_objects)
+        
