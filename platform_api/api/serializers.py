df286851f84c6fa05d9cd2bfcc7346476243455f
Joseph Braide <braidej@gmail.com>
2023-07-19 18:47:00 +0100

    crypto and Fiat Serializers

--- platform_api/api/serializers.py
+++ platform_api/api/serializers.py
@@ -1,7 +1,32 @@
-from rest_framework.serializers import ModelSerializer
-from .models import CryptoWallet
+from rest_framework import serializers
+from .models import CryptoWallet, FiatWallet
 
-class CryptoWalletSerializer(ModelSerializer):
+###########
+#
+##########
+class CryptoWalletSerializer(serializers.ModelSerializer):
     class Meta:
         model = CryptoWallet
-        fields = ['wallet_type']
+        exclude = ['user', 'id']
+
+class FiatWalletSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = FiatWallet
+        exclude = ['user', 'id']
+
+###########
+# Crypto trading serializers
+##########
+class CryptoPurchaseSerializer(serializers.Serializer):
+    """
+    Serializer should be used in cases of Buying/Selling crypto
+    Assets
+    """
+    crypto_to_purchase = serializers.CharField()
+    quantity_of_crypto = serializers.DecimalField(decimal_places=8, max_digits=16)
+    payment_method = serializers.CharField()
+    price = serializers.DecimalField(decimal_places=8, max_digits=16)
+
+class FundFiatWalletSerializer(serializers.Serializer):
+    amount = serializers.DecimalField(max_digits=10, decimal_places=2)
+    wallet_to_deposit = serializers.CharField()
