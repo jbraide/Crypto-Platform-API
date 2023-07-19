96dcf450cd06358b1b8b3028f952c7d7494c0ccd
Joseph Braide <braidej@gmail.com>
2023-07-19 18:57:32 +0100

    endpoint urls

--- platform_api/api/urls.py
+++ platform_api/api/urls.py
@@ -1,8 +1,29 @@
 from django.urls import path
-from .views import CreateBaseCryptoWallets
+from .views import (
+    CreateBaseCryptoWallets, 
+    DashboardHomeView, 
+    GetCryptoMarketPriceAPI, 
+    GetKlinePrices,
+    BuyCryptoCurrency,
+    SellCryptoCurrency,
+    DepositToFiatWallet
+    )
 
 app_name = 'platform-api'
 
 urlpatterns = [
-    path('create-base-crypto-wallets/', CreateBaseCryptoWallets.as_view())    
+    # dashboard
+    path('dashboard/', DashboardHomeView.as_view()),
+
+    # wallets
+    path('create-base-crypto-wallets/', CreateBaseCryptoWallets.as_view()),
+
+    # market information
+    path('crypto-market-pricing/', GetCryptoMarketPriceAPI.as_view()),
+    path('klines-pricing/',GetKlinePrices.as_view(),),
+
+    # Crypto Trading
+    path('buy-crypto/', BuyCryptoCurrency.as_view(),),
+    path('sell-crypto/', SellCryptoCurrency.as_view(),),
+    path('deposit/fiat/', DepositToFiatWallet.as_view())
 ]
