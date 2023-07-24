1fafea0e8f205634f2ae05bd9760cb28b2ab97a9
Joseph Braide <braidej@gmail.com>
2023-07-24 14:23:05 +0100

    PRODUCTION CONFIGS

--- authentication/authentication/settings_prod.py
+++ authentication/authentication/settings_prod.py
@@ -1,1 +1,16 @@
 from .settings import *
+from dotenv import load_dotenv
+load_dotenv()
+
+ALLOWED_HOSTS = ['microepay.com', '31.172.83.205']
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv('DB_NAME'),
+        'USER': os.getenv('DB_USER'),
+        'PASSWORD': os.getenv('DB_PASSWORD'),
+        'HOST': os.getenv('DB_HOST'),
+        'PORT': '',
+    }
+}
