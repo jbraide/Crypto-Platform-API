6ef73c344cc95bb87ae38d5e0d34fefc2ec35b15
Joseph Braide <braidej@gmail.com>
2023-07-24 07:31:03 +0100

    updated to support dev settings file

--- invoice_transactions/manage.py
+++ invoice_transactions/manage.py
@@ -6,7 +6,7 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'invoice_transactions.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'invoice_transactions.settings_dev')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
